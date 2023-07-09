919fcadede841acf30eabb434d27e18902266130
debojitsaha <saha05debojit@gmail.com>
2023-07-09 21:02:14 +0000

    Fixed: ML Model & script variables global

--- scripts/script.py
+++ scripts/script.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import pandas as pd
 
-model = pickle.load(open('scripts/model.pkl', 'rb'))
+model = pickle.load(open('scripts/finalized_model.pkl', 'rb'))
 
 Cylinders=0.0
 Engine_Size=0.0
@@ -12,6 +12,7 @@ Fuel_Consumption_Comb=0.0
 Fuel_Consumption_Comb_mpg=0.0
 Fuel_Consumption_Hwy=0.0
 Fuel_Type_E=0
+Fuel_Type_N=0
 Fuel_Type_X=0
 Fuel_Type_Z=0
 Make_Type_Luxury=0
@@ -53,13 +54,55 @@ def get_prediction(engine, cylinder, city, highway, combination, combination_mpg
     prepare_data(engine, cylinder, city, highway, combination, combination_mpg, make, vehicle, transmission, fuel)
 
     # step 2. prepare dataframe to store matrix
-    c=pd.DataFrame([Engine_Size, Cylinders, Fuel_Consumption_City,Fuel_Consumption_Hwy, Fuel_Consumption_Comb,Fuel_Consumption_Comb_mpg,Fuel_Type_E, Fuel_Type_X,Fuel_Type_Z, Transmission_A4, Transmission_A5, Transmission_A6,Transmission_A7, Transmission_A8, Transmission_A9,Transmission_AM5, Transmission_AM6, Transmission_AM7,Transmission_AM8, Transmission_AM9, Transmission_AS10,Transmission_AS4, Transmission_AS5, Transmission_AS6,Transmission_AS7, Transmission_AS8, Transmission_AS9,Transmission_AV, Transmission_AV10, Transmission_AV6,Transmission_AV7, Transmission_AV8, Transmission_M5,Transmission_M6, Transmission_M7, Make_Type_Luxury,Make_Type_Premium, Make_Type_Sports, Vehicle_Class_Type_SUV,Vehicle_Class_Type_Sedan, Vehicle_Class_Type_Truck]).T
+    c=pd.DataFrame([Engine_Size, Cylinders, Fuel_Consumption_City,Fuel_Consumption_Hwy, Fuel_Consumption_Comb,Fuel_Consumption_Comb_mpg,Fuel_Type_E, Fuel_Type_N, Fuel_Type_X,Fuel_Type_Z, Transmission_A4, Transmission_A5, Transmission_A6,Transmission_A7, Transmission_A8, Transmission_A9,Transmission_AM5, Transmission_AM6, Transmission_AM7,Transmission_AM8, Transmission_AM9, Transmission_AS10,Transmission_AS4, Transmission_AS5, Transmission_AS6,Transmission_AS7, Transmission_AS8, Transmission_AS9,Transmission_AV, Transmission_AV10, Transmission_AV6,Transmission_AV7, Transmission_AV8, Transmission_M5,Transmission_M6, Transmission_M7, Make_Type_Luxury,Make_Type_Premium, Make_Type_Sports, Vehicle_Class_Type_SUV,Vehicle_Class_Type_Sedan, Vehicle_Class_Type_Truck]).T
 
     # step 3. call model.predict() by passing the above dataframe
     prediction = model.predict(c)
     return np.round(prediction,2)
 
 def prepare_data(engine, cylinder, city, highway, combination, combination_mpg, make, vehicle, transmission, fuel):
+    global Cylinders
+    global Engine_Size
+    global Fuel_Consumption_City
+    global Fuel_Consumption_Comb
+    global Fuel_Consumption_Comb_mpg
+    global Fuel_Consumption_Hwy
+    global Fuel_Type_E
+    global Fuel_Type_N
+    global Fuel_Type_X
+    global Fuel_Type_Z
+    global Make_Type_Luxury
+    global Make_Type_Premium
+    global Make_Type_Sports
+    global Transmission_A4
+    global Transmission_A5
+    global Transmission_A6
+    global Transmission_A7
+    global Transmission_A8
+    global Transmission_A9
+    global Transmission_AM5
+    global Transmission_AM6
+    global Transmission_AM7
+    global Transmission_AM8
+    global Transmission_AM9
+    global Transmission_AS10
+    global Transmission_AS4
+    global Transmission_AS5
+    global Transmission_AS6
+    global Transmission_AS7
+    global Transmission_AS8
+    global Transmission_AS9
+    global Transmission_AV
+    global Transmission_AV10
+    global Transmission_AV6
+    global Transmission_AV7
+    global Transmission_AV8
+    global Transmission_M5
+    global Transmission_M6
+    global Transmission_M7
+    global Vehicle_Class_Type_Sedan
+    global Vehicle_Class_Type_SUV
+    global Vehicle_Class_Type_Truck
     Cylinders=float(cylinder)
     Engine_Size=float(engine)
     Fuel_Consumption_City=float(city)
@@ -68,6 +111,8 @@ def prepare_data(engine, cylinder, city, highway, combination, combination_mpg, 
     Fuel_Consumption_Comb_mpg=float(combination_mpg)
     if fuel == "Fuel_Type_E":
         Fuel_Type_E=1
+    if fuel == "Fuel_Type_N":
+        Fuel_Type_N=1
     if fuel == "Fuel_Type_X":
         Fuel_Type_X=1
     if fuel == "Fuel_Type_Z":
