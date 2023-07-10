0252544c7de8c64993523e353df17add1e9a0865
debojitsaha <saha05debojit@gmail.com>
2023-07-10 03:53:23 +0000

    Updated: CORS Policy

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,9 @@ import cors from "cors";
 import express from "express";
 
 // Import custom packages we'll be using
-import {
-    connect,
-    getConnectionState,
-} from "./config/db.config";
+import { connect, getConnectionState } from "./config/db.config";
 import { mainRouter } from "./routes/main.route";
 import { ErrorHandler } from "./controllers/error.controller";
-
 
 const MONGO_URI = String(process.env.CONNECTION_URI);
 const PORT = Number(process.env.PORT);
@@ -19,7 +15,13 @@ const PORT = Number(process.env.PORT);
 const app: express.Application = express();
 app.disable("x-powered-by");
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -28,11 +30,11 @@ app.use(ErrorHandler);
 app.use("/", mainRouter);
 
 app.listen(PORT, async () => {
-    const db = await connect(MONGO_URI);
-    console.log(
-        `${getConnectionState(db.connection.readyState)} to the database`
-    );
-    console.log(`Listening on localhost:${PORT}...`);    
+  const db = await connect(MONGO_URI);
+  console.log(
+    `${getConnectionState(db.connection.readyState)} to the database`
+  );
+  console.log(`Listening on localhost:${PORT}...`);
 });
 
 export { app };
